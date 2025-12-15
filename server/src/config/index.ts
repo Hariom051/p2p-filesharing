fa8cfe61fa06071e1f367642dc02528bbacb65dd
Hariom <hariom@Harioms-MacBook-Air.local>
2025-12-15 15:37:11 +0530

    changes

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -2,6 +2,6 @@ export const app = {
   dbUrl: process.env["DB_URL"],
   allowedOrigins:
     process.env["IS_PRODUCTION"] === "true"
-      ? ["xerrasend.onrender.app"]
+      ? ["xerrasend.vercel.app"]
       : ["http://localhost:3000"],
 };
